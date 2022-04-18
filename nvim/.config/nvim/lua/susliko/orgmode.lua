a6a83fc7daac97a3aa42f13c05135996247d4ccf
Vasiliy Morkovkin <1istoobig@gmail.com>
2022-04-18 10:38:33 +0300

    nvim: guard orgmode

--- nvim/.config/nvim/lua/susliko/orgmode.lua
+++ nvim/.config/nvim/lua/susliko/orgmode.lua
@@ -1,3 +1,8 @@
+local orgmode_ok, _ = pcall(require, "orgmode")
+if not orgmode_ok then 
+  return
+end
+
 require('orgmode').setup({
   org_agenda_files = {'~/diary/*'},
   org_default_notes_file = '~/diary/default.org',
