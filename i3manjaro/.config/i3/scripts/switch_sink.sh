8d5e83fdac04c2c30230daf926c70e07c474df8f
Vasiliy Morkovkin <1istoobig@gmail.com>
2022-06-05 20:53:40 +0300

    configure rofi

--- i3manjaro/.config/i3/scripts/switch_sink.sh
+++ i3manjaro/.config/i3/scripts/switch_sink.sh
@@ -7,6 +7,6 @@ default_sink=$(pactl info | grep 'Default Sink' | cut -d : -f 2)
 
 active_sink=$(echo "$all_sinks" | grep -n $default_sink | cut -d : -f 1)
 
-selected_sink=$(echo "$all_sinks" | rofi -dpi 1 -dmenu -i -a $(($active_sink - 1)) -p 'Select a device: ')
+selected_sink=$(echo "$all_sinks" | rofi -dmenu -i -a $(($active_sink - 1)) -p 'Select a device: ')
 
 pactl set-default-sink $selected_sink
