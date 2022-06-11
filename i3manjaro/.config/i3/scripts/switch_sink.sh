836d35b3d0f940affcef2b8664f0fcb9d3b34585
susliko <1istoobig@gmail.com>
2022-06-11 21:58:41 +0300

    shorten sink names
    .

--- i3manjaro/.config/i3/scripts/switch_sink.sh
+++ i3manjaro/.config/i3/scripts/switch_sink.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
-cut
 all_sinks=$(pactl list short sinks | cut -f 2)
+all_short_sinks=$(pactl list short sinks | cut -f 2 |  sed 's/.analog-stereo//' | sed 's/alsa_output.//')
+echo "$all_short_sinks"
 
 default_sink=$(pactl info | grep 'Default Sink' | cut -d : -f 2)
 
 active_sink=$(echo "$all_sinks" | grep -n $default_sink | cut -d : -f 1)
 
-selected_sink=$(echo "$all_sinks" | rofi -dmenu -i -a $(($active_sink - 1)) -p 'Select a device: ')
+selected_short_sink=$(echo "$all_short_sinks" | rofi -dmenu -i -a $(($active_sink - 1)) -p 'Select a device: ')
 
-pactl set-default-sink $selected_sink
+short_sink=$(echo "$all_sinks" | grep "$selected_short_sink")
+
+pactl set-default-sink $short_sink
